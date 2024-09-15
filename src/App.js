db488daeab8ef68091e8b3ec90a977a62f2ef525
nekonomicus <Samuel.schaible@gmail.com>
2024-09-15 19:59:28 +0200

    Simplify App component and update index files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Navigation from './components/Navigation';
-import Challenge from './pages/Challenge';
-import Proposal from './pages/Proposal';
-import Technologies from './pages/Technologies';
-import ScientificBasis from './pages/ScientificBasis';
-import Advantages from './pages/Advantages';
-import Opportunity from './pages/Opportunity';
-import Budget from './pages/Budget';
-import Future from './pages/Future';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-gray-100">
-        <Header />
-        <Navigation />
+        <header className="bg-blue-600 text-white p-4">
+          <h1 className="text-2xl font-bold">VR Surgery Training App</h1>
+        </header>
         <main className="flex-grow container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Challenge />} />
-            <Route path="/proposal" element={<Proposal />} />
-            <Route path="/technologies" element={<Technologies />} />
-            <Route path="/scientific-basis" element={<ScientificBasis />} />
-            <Route path="/advantages" element={<Advantages />} />
-            <Route path="/opportunity" element={<Opportunity />} />
-            <Route path="/budget" element={<Budget />} />
-            <Route path="/future" element={<Future />} />
-          </Routes>
+          <p className="text-xl">Welcome to our VR Surgery Training App pitch!</p>
         </main>
-        <Footer />
+        <footer className="bg-gray-200 p-4 text-center">
+          <p>&copy; 2023 VR Surgery Training App</p>
+        </footer>
       </div>
     </Router>
   );
